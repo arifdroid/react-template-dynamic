67cef7611d6ffb2ab2e7fd1ced4fb1c18a1661d1
arifdroid <arifdroiddeveloper@gmail.com>
2020-11-28 11:24:45 +0800

    working forward ref

--- src/view/sample-form-page-2/Input/Input.tsx
+++ src/view/sample-form-page-2/Input/Input.tsx
@@ -3,14 +3,18 @@ import React,{forwardRef} from "react";
 
 interface Props{
     type:string,
-    name:string
+    name:string,
+    error:any,
+    helperText:string
 }
 
-const Input =({type,name, ...props}:Props,ref:any)=>{
+const Input =({type,name,error,helperText, ...props}:Props,ref:any)=>{
     return <TextField
     inputRef={ref} 
     type={type}
     name={name}
+    helperText={helperText}
+    error={error}
     variant='outlined' margin='normal'
     {...props}></TextField>
 }
--- src/view/sample-form-page-2/SampleFormPage2.tsx
+++ src/view/sample-form-page-2/SampleFormPage2.tsx
@@ -4,22 +4,36 @@ import ContentContainer from './content/ContentContainer';
 import Form from './Form/Form';
 import ForwardInput from './Input/Input';
 import Button from './button/Button'
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers';
+
 
 interface Props{
 
 }
 
+const schema = yup.object().shape({
+    firstname: yup.string().matches(/^[A-Za-z]+$/i
+        , "first name should not contain numbers")
+    .required('First name is required field'),
+    surname:  yup.string().matches(/^[A-Za-z]+$/i, "last name should not contain numbers")
+    .required('Last name is required field'),
+})
+
 const SampleFormPage2 =(props:Props)=>{
     // console.log('apa ada dalam props', props)
 
-    const {register} = useForm();
+    const {register, handleSubmit, errors} = useForm({
+        mode:'onBlur',
+        resolver: yupResolver(schema)
+    });
 
     return (
         <ContentContainer>
             <div>Form Page</div>
             <Form>
-            <ForwardInput name='firstname' type='text' ref={register}></ForwardInput>
-            <ForwardInput name='surname' type='text' ref={register}></ForwardInput>
+            <ForwardInput name='firstname' type='text' ref={register} error={!!errors.firstname} helperText={errors?.firstname?.message}></ForwardInput>
+            <ForwardInput name='surname' type='text' ref={register} error={!!errors.surname} helperText={errors?.surname?.message}></ForwardInput>
 
             <Button >Next</Button>
                 
